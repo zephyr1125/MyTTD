5b0c3588d49d58e9f863ccbfba194950839f17e9
Zephyr@home <zephyr1125@gmail.com>
2023-06-13 14:44:27 +0800

    successfully run the first window!

---
+++ build.bat
@@ -0,0 +1,2 @@
+@echo off
+g++ -ISDL2/include -LSDL2/lib/x64 main.cpp -o main -lSDL2
--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* args[]) {
+    SDL_SetMainReady();
+    
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Hello, SDL!",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        640,
+        480,
+        SDL_WINDOW_SHOWN
+    );
+
+    SDL_Delay(2000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
